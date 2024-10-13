6cb8e62ca0becadcee72cc03a57643d0cdc67c24
Mohmmad Burair <aliburair87@gmail.com>
2024-10-13 10:53:52 +0500

    Update question01.c

--- PF_LAB_assignments24k-0775/lab07/question01.c
+++ PF_LAB_assignments24k-0775/lab07/question01.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+int main(){
+	int count=0 , sum=0 , i;
+	printf("How many numbers you want to sum: ");
+	scanf("%d",&count);
+	
+	int arr[count];
+	
+	for(i=0; i<count; i++){
+		printf("\nEnter %d number :",i+1);
+		scanf("%d",&arr[i]);
+		sum+=arr[i];
+	}
+	printf("\n{");
+	for(i=0; i<count; i++){
+		printf("%d",arr[i]);
+		if(i<count-1)
+			printf(",");
+	}
+	printf("}\n");
+	printf("The sum of the given numbers is : %d",sum);
 
+	return 0;
+}
