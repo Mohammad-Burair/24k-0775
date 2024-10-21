83cf971fe1477309c63a29d9c1e79f3e5531b758
Mohmmad Burair <aliburair87@gmail.com>
2024-10-21 08:49:09 +0500

    Update question1.c

--- PF_LAB_assignments24k-0775/lab08/question1.c
+++ PF_LAB_assignments24k-0775/lab08/question1.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
 
+int main(){
+
+    int i , j , height , width;
+
+    printf("Enter the height of the rigt trianlge: \n");
+    scanf("%d",&height);
+
+
+    for(i = 0; i <= height; i++){
+        for(j = 0; j <= height; j++){
+            if(i>j){
+                printf("%d",j+1);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
