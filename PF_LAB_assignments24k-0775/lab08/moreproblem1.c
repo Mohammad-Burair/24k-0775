2aaaeda3e11e8b8ff5f3689345b86ee1bfbeee92
Mohmmad Burair <aliburair87@gmail.com>
2024-10-21 10:21:16 +0500

    Add files via upload

---
+++ PF_LAB_assignments24k-0775/lab08/moreproblem1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+
+    int start , end , i , j , isprime = 1 ;
+
+    printf("Enter the starting number: \n");
+    scanf("%d",&start);
+
+    printf("Enter the ending number: \n");
+    scanf("%d",&end);
+
+    for(i = start ; i <= end; i++){
+        if(i == 2){
+            printf(" %d ", i);
+        }
+        for(j = 2; j < i; j++){
+            isprime=1;
+            if(i%j == 0){
+                isprime = 0;
+                break;
+            }
+        }
+        if(isprime == 1){
+            printf(" %d ", i);
+        } 
+    }
+
+    return 0;
+}
