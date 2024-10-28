ceb119eba36f3e9690526b56bc831d2e55950e05
Mohmmad Burair <aliburair87@gmail.com>
2024-10-28 08:22:02 +0500

    Update functionproblem1.c

--- PF_LAB_assignments24k-0775/lab09/functionproblem1.c
+++ PF_LAB_assignments24k-0775/lab09/functionproblem1.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int product(int a , int b){
+    return a*b;
+}
+
+int main(){
+    
+    int a , b , mul;
+
+    printf("Enter any two numbers : ");
+    scanf("%d %d",&a , &b);
+    mul = product(a, b);
+
+    printf("The product of %d and %d is %d", a, b , mul);
+
+    return 0;
+}   
