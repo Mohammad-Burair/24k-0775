6a4c2be2e38a385bb2949ce65ab86cf994c69c6f
Mohmmad Burair <aliburair87@gmail.com>
2024-11-15 12:34:57 +0500

    Update recursionsectionprob1.c

--- PF_LAB_assignments24k-0775/lab10/recursionsectionprob1.c
+++ PF_LAB_assignments24k-0775/lab10/recursionsectionprob1.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int digitsum(int a){
 
+    if(a == 0)
+        return 0;
+
+    return a%10 + digitsum(a/10);
+}
+int main(){
+    int a , result;
+    printf("Enter any number : ");
+    scanf("%d",&a);
+    result = digitsum(a);
+    printf("The sum of digit of given number is : %d", result);
+    return 0;
+}
