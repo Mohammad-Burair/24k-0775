fde8f217c6ab342432a133d6f71b1d09d70f6419
Mohmmad Burair <aliburair87@gmail.com>
2024-11-18 09:35:46 +0500

    Update structuresectionprob1.c

--- PF_LAB_assignments24k-0775/lab10/structuresectionprob1.c
+++ PF_LAB_assignments24k-0775/lab10/structuresectionprob1.c
@@ -1,1 +1,58 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
 
+struct file_info {
+    int flight_number; 
+    char depart_city[100];
+    char dest_city[100];
+    int req_seats;
+    int available_seat;
+    int booking_number;
+
+};
+
+int main(){
+
+    srand(time(NULL));  
+
+    struct file_info flight;
+
+    flight.available_seat = rand() % 100 + 1;
+
+    printf("Enter the number of the flight: ");
+    scanf("%d", &flight.flight_number);
+    getchar();
+
+    printf("Enter the departure city: ");
+    fgets(flight.depart_city , 100 , stdin);
+    flight.depart_city[strcspn(flight.depart_city , "\n")] = '\0';
+
+    printf("Enter the destination city: ");
+    fgets(flight.dest_city , 100 , stdin);
+    flight.dest_city[strcspn(flight.dest_city , "\n")] = '\0';
+    
+enterseats:
+    printf("Enter the number of seats required: ");
+    scanf("%d",&flight.req_seats);
+
+    printf("The seats available are : %d\n", flight.available_seat);
+
+    if(flight.req_seats <= flight.available_seat){
+        flight.booking_number = rand() % 50000 +123;
+        printf("The flight is available.\n");
+        printf("Your details :\n");
+        printf("Flight Number: %d \n", flight.flight_number);
+        printf("Your departure city : %s \n", flight.depart_city);
+        printf("Your destination city : %s \n", flight.dest_city);
+        printf("Your seats : %d\n", flight.req_seats);
+        printf("Your booking number : %d\n", flight.booking_number);
+    }
+
+    else {
+        printf("The required seats by y ou are not available.\n");
+        goto enterseats;
+    }
+
+}
