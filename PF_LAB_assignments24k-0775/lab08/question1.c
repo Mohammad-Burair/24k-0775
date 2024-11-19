38ddaaa9f8169d62c32f7317705f28131c7c8279
Mohmmad Burair <aliburair87@gmail.com>
2024-11-19 13:32:20 +0500

    Update question1.c

--- PF_LAB_assignments24k-0775/lab08/question1.c
+++ PF_LAB_assignments24k-0775/lab08/question1.c
@@ -1,3 +1,23 @@
+/*
+
+Problem Statement:
+Write a C program that takes the height of a right-angled triangle as input and prints a pattern of numbers, where the numbers in each row increase from 1 up to the row number, forming a right triangle.
+
+ Input:
+ An integer `height` representing the height of the triangle.
+
+ Output:
+ A right-angled triangle pattern with numbers, where the numbers in each row increment sequentially.
+
+Sample Output:
+
+For `height = 5`, the output will be:
+1
+12
+123
+1234
+12345
+*/
 #include<stdio.h>
 
 int main(){
